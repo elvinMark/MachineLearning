8f05e5c86daf3dad2d5ec5f4bd45480229f37057
elvinMark <elvin.mark.titech@gmail.com>
2020-10-28 16:53:33 +0900

    Adding some Activation functions and Loss functions as well as a softmax block

--- SimpleFCC.py
+++ SimpleFCC.py
@@ -9,17 +9,39 @@ Activation Functions
 """
 def sigmoid(x,diff=False):
     if diff:
-        x*(1-x)
+        return x*(1-x)
     return 1/(1 + np.exp(-x))
 
 def tanh(x,diff=False):
     if diff:
-        (1 - x**2)/2
+        return (1 - x**2)/2
     return (1 - np.exp(-x))/(1 + np.exp(-x))
 
-dict_act_fun = {"sigmoid":sigmoid,"tanh":tanh}
+def relu(x,diff=False):
+    tmp = np.zeros(x.shape)
+    if diff:
+        tmp[np.where(x>0)] = 1
+        return tmp
+    tmp[np.where(x>0)] = x[np.where(x>0)]
+    return tmp
 
-class Block:
+def leaky_relu(x,diff=False):
+    if diff:
+        tmp = np.ones(x.shape)
+        tmp[np.where(x<=0)] = 0.01
+        return tmp
+    tmp = 0.01*x
+    tmp[np.where(x>0)] = x[np.where(x>0)]
+    return tmp
+
+def linear(x,diff=False):
+    if diff:
+        return np.ones(x.shape)
+    return x
+
+dict_act_fun = {"sigmoid":sigmoid,"tanh":tanh,"relu":relu,"leaky_relu":leaky_relu,"linear":linear}
+
+class BlockFCC:
     def __init__(self,n_inputs,n_outputs,act_fun="sigmoid"):
         self.weights  = np.random.random((n_inputs,n_outputs))
         self.bias = np.random.random(n_outputs)
@@ -36,17 +58,39 @@ class Block:
         self.bias = self.bias - alpha*self.delta.sum(axis=0)
 
 
+class BlockSoftmax:
+    def forward(self,inp_data):
+        self.out = np.exp(inp_data)
+        n = len(self.out)
+        self.out = self.out/(self.out.sum(axis=1).reshape(n,1))
+        return self.out
+    def backward(self,err):
+        tmp = self.out * err
+        n = len(tmp)
+        tmp = tmp - self.out*(tmp.sum(axis=1).reshape(n,1))
+        return tmp
+    def update(self,alpha=1):
+        pass
+    
 """
 Loss Functions
 """
-def MSE(out,target):
+def MSE(out,target): # Mean Square Error
     err = out - target
     loss = 0.5*err*err
     return loss.sum(),err
 
+def CE(out,target): # Creoss Entropy Error
+    loss = -target*np.log(out)
+    err = -target/out
+    return loss.sum(),err
+
+dict_loss_function = {"MSE":MSE,"CE":CE}
+
 class Model:
-    def __init__(self):
+    def __init__(self,loss_function="MSE"):
         self.blocks = []
+        self.loss_function = dict_loss_function[loss_function]
     def add(self,block):
         self.blocks.append(block)
     def forward(self,inp_data):
@@ -55,7 +99,7 @@ class Model:
             self.out = block.forward(self.out)
         return self.out
     def calculate_loss(self,target_data):
-        return MSE(self.out,target_data)
+        return self.loss_function(self.out,target_data)
     def backward(self,err):
         err_tmp = err
         for block in reversed(self.blocks):
@@ -63,6 +107,8 @@ class Model:
     def update(self,alpha=1):
         for block in self.blocks:
             block.update(alpha=alpha)
+    def set_loss_function(self,loss_function):
+        self.loss_function = dict_loss_function[loss_function]
     def train(self,inp_data,target_data,maxIt = 1000,alpha=1):
         self.loss = []
         for i in range(maxIt):
@@ -76,10 +122,15 @@ class Model:
 if __name__ == "__main__":
     # Test of FCC: XOR gate
     model = Model()
-    model.add(Block(2,3))
-    model.add(Block(3,2))
+    model.add(BlockFCC(2,3,act_fun="sigmoid"))
+    model.add(BlockFCC(3,5,act_fun="tanh"))
+    model.add(BlockFCC(5,2,act_fun="relu"))
+    model.add(BlockSoftmax())
+    model.set_loss_function("CE")
     inp_data = np.array([[0,0],[1,0],[0,1],[1,1]])
     out_data = np.array([[1,0],[0,1],[0,1],[1,0]])
-    model.train(inp_data,out_data)
+    model.train(inp_data,out_data,alpha=0.1,maxIt=5000)
     pred = model.predict(inp_data)
     print(pred)
+    plt.plot(np.array(model.loss))
+    plt.show()
