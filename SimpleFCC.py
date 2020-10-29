8466e1d56282fb5e2d63d648ce59bd59fb0fdfc1
Elvin Mark <elvin.mark.titech@gmail.com>
2020-10-29 12:24:46 +0900

    Adding some comments to Simple FCC

--- SimpleFCC.py
+++ SimpleFCC.py
@@ -1,11 +1,29 @@
 """
 Simple Fully Connected Neural Network
 """
+
+"""First we need to import some useful libraries: numpy ans matplotlib
+
+- numpy will allow us to work with matrices that we are going to use a lot in
+  the code
+
+- matplotlib will allow us to draw some graphs like the how the loss functions
+  is evolving during the training
+
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 """
 Activation Functions
+
+We will define here some activation (the more famous ones) functions.
+
+- Sigmoid
+- Hyperbolic tangent
+- Rectified linear unit (ReLU)
+- Leaky Rectified Linear Unit (Leaky ReLU)
+- Linear 
 """
 def sigmoid(x,diff=False):
     if diff:
@@ -39,8 +57,17 @@ def linear(x,diff=False):
         return np.ones(x.shape)
     return x
 
-dict_act_fun = {"sigmoid":sigmoid,"tanh":tanh,"relu":relu,"leaky_relu":leaky_relu,"linear":linear}
+# This dictionary will allows us later to access the activation functions
+dict_act_fun = {"sigmoid":sigmoid,"tanh":tanh,"relu":relu,"leaky_relu":leaky_relu,"linear":linear} 
 
+"""
+Block Class
+
+The way I see neural networks is as a series of blocks. Each Block is
+representing the connection between 2 layers. The block will computer the input
+and give an output.
+
+"""
 class BlockFCC:
     def __init__(self,n_inputs,n_outputs,act_fun="sigmoid"):
         self.weights  = np.random.random((n_inputs,n_outputs))
@@ -74,6 +101,12 @@ class BlockSoftmax:
     
 """
 Loss Functions
+
+Here I am defining some Loss Functions. The 2 more famous one (or at least the
+ones I've learned so far) are the following 2.
+
+- Mean Square Error
+- Cross Entropy 
 """
 def MSE(out,target): # Mean Square Error
     err = out - target
@@ -85,8 +118,15 @@ def CE(out,target): # Creoss Entropy Error
     err = -target/out
     return loss.sum(),err
 
+# This dictionary will allow us to select the loss function we want
 dict_loss_function = {"MSE":MSE,"CE":CE}
 
+"""Model class
+
+This class contains a series of blocks that made the actual neural network. This
+class also contains some methods to train the neural network.
+
+"""
 class Model:
     def __init__(self,loss_function="MSE"):
         self.blocks = []
@@ -119,18 +159,52 @@ class Model:
             self.update(alpha=alpha)
     def predict(self,inp_data):
         return self.forward(inp_data)
+
+import sys 
+    
 if __name__ == "__main__":
+    # Test: Classifying points
+    if sys.argv[1] == "1":
+        # Creating the neural network (model)
+        model = Model()
+        model.add(BlockFCC(2,5,act_fun="tanh"))
+        model.add(BlockFCC(5,2,act_fun="relu"))
+        model.add(BlockSoftmax())
+        model.set_loss_function("CE")
+        # Creating the training data
+        N = 10 # Sample size
+        x1 = 5 * np.random.random(N) - 2
+        y1 = 5 * np.random.random(N) - 2
+        C1 = [1,0] # Class 1
+        C2 = [0,1] # Class 2
+        inp_data = np.array([[i,j] for i,j in zip(x1,y1)])
+        out_data = np.array([C1 if i<j else C2 for i,j in inp_data])
+        # Training
+        model.train(inp_data,out_data,maxIt=1000,alpha=0.1)
+        # Predecting
+        pred = model.predict(inp_data)
+        for i,j in zip(pred,out_data):
+            print(i,j)
+        # Plotting the loos evolution while training
+        plt.plot(model.loss)
+        plt.show()
     # Test of FCC: XOR gate
-    model = Model()
-    model.add(BlockFCC(2,3,act_fun="sigmoid"))
-    model.add(BlockFCC(3,5,act_fun="tanh"))
-    model.add(BlockFCC(5,2,act_fun="relu"))
-    model.add(BlockSoftmax())
-    model.set_loss_function("CE")
-    inp_data = np.array([[0,0],[1,0],[0,1],[1,1]])
-    out_data = np.array([[1,0],[0,1],[0,1],[1,0]])
-    model.train(inp_data,out_data,alpha=0.1,maxIt=5000)
-    pred = model.predict(inp_data)
-    print(pred)
-    plt.plot(np.array(model.loss))
-    plt.show()
+    elif sys.argv[2] == "2":
+        # Creating the neural network (model)
+        model = Model()
+        model.add(BlockFCC(2,3,act_fun="sigmoid"))
+        model.add(BlockFCC(3,5,act_fun="tanh"))
+        model.add(BlockFCC(5,2,act_fun="relu"))
+        model.add(BlockSoftmax())
+        model.set_loss_function("CE")
+        # Creating the training data
+        inp_data = np.array([[0,0],[1,0],[0,1],[1,1]])
+        out_data = np.array([[1,0],[0,1],[0,1],[1,0]])
+        # Training
+        model.train(inp_data,out_data,alpha=0.1,maxIt=5000)
+        # Predicting
+        pred = model.predict(inp_data)
+        print(pred)
+        # Plotting the loss evolution while training 
+        plt.plot(np.array(model.loss))
+        plt.show()
